92c4a894c081893d310a86d8292527e7dddf31e0
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-17 13:48:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+    let result = [];
+    for (let i = 0; i < names.length; i++){
+        result.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+
+    return result;
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
